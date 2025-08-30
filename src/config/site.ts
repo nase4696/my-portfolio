fff59bea8c89b91d223454a759fbc52bda7168ae
有川瀬名 <arikawasena@arikawasenanoMacBook-Air.local>
2025-08-30 17:30:30 +0900

    Next.js プロジェクト初期化

---
+++ src/config/site.ts
@@ -0,0 +1,12 @@
+import { SiteConfig } from "@/types/config";
+
+export const siteConfig: SiteConfig = {
+  name: "My Portfolio",
+  description: "Next.jsで作成したポートフォリオサイトです",
+  url: "http://localhost:3000",
+  ogImage: "",
+  links: {
+    x: "",
+    github: "",
+  },
+};
---
+++ src/types/config.d.ts
@@ -0,0 +1,24 @@
+export type NavItem = {
+  title: string;
+  href: string;
+  disabled?: boolean;
+};
+
+export type MainNavConfig = {
+  mainNav: NavItem[];
+};
+
+export type MobileNavConfig = {
+  mobileNav: NavItem[];
+};
+
+export type SiteConfig = {
+  name: string;
+  description: string;
+  url: string;
+  ogImage: string;
+  links?: {
+    x: string;
+    github: string;
+  };
+};
